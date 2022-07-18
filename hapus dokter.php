8213c212db877acd17a9a2e8d5fb137097533028
Imam Fauzi(Bangimm) <106543547+imamfauzi284@users.noreply.github.com>
2022-07-19 00:42:59 +0700

    Delete hapus dokter.php

--- hapus dokter.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include '../koneksi.php';
-	$id = $_GET['id'];
-	$sql = "DELETE FROM dokter WHERE id_dokter = '{$id}'";
-	$result = mysqli_query($conn, $sql);
-	header('location: dokter.php');
-?

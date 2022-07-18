3c2f2e0f0b9968f2fb257e8830b15f684f63fa5f
Imam Fauzi(Bangimm) <106543547+imamfauzi284@users.noreply.github.com>
2022-07-19 00:42:43 +0700

    Delete hapus pasien.php

--- hapus pasien.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include '../koneksi.php';
-	$id = $_GET['id'];
-	$sql = "DELETE FROM pasien WHERE id_pasien = '{$id}'";
-	$result = mysqli_query($conn, $sql);
-	header('location: pasien.php');
-?>
